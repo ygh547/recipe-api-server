26d574e9ea700bf505a1510eae370cc630c80e28
ygh547 <ygh547@naver.com>
2022-06-17 13:03:29 +0900

    add

--- mysql_connection.py
+++ mysql_connection.py
@@ -1,2 +1,10 @@
-import mysuq.connector
+import mysql.connector
 
+def get_connection() :
+    connection = mysql.connector.connect(
+        host = 'jhdb.ct294vqgyq74.ap-northeast-2.rds.amazonaws.com',
+        database = 'recipe_db',
+        user = 'recipe_user',
+        password = 'recipe1234'
+    )
+    return connection
