0d731f143458e4c022b9969dc158a58390ec6642
ygh547 <ygh547@naver.com>
2022-06-17 15:48:57 +0900

    add

--- resources/recipe.py
+++ resources/recipe.py
@@ -1,4 +1,5 @@
 from http import HTTPStatus
+from unittest import result
 from flask import request
 from flask_restful import Resource
 from mysql.connector.errors import Error
@@ -10,10 +11,6 @@ import mysql.connector
 # 클래스는 상속이 가능하다.
 # API를 만들기 위한 클래스는, flask_restful 라이브러리의 Resorce 클래스를 상속해서 만들어야 한다.
 
-# name = '순두부'
-# description = '순두부찌개 만드는법'
-# cook_time = 45
-# directions = '물넣고 두부넣고 조개넣고 끓인다'
 
 class RecipeListResource(Resource):
     # restful api의 method에 해당하는 함수 작성
@@ -52,3 +49,55 @@ class RecipeListResource(Resource):
             return {"error" : str(e)}, 503
 
         return {"result" : "success"}, 200
+
+    
+    def get(self):
+        # 쿼리 스트링으로 오는 데이터는 아래처럼 처리해준다.
+        offset = request.args.get('offset')
+        limit = request.args.get('limit')
+        
+        # db로부터, 데이터를 받아서, 클라이언트에 보내준다.
+        try :
+            connection = get_connection()
+
+            query = '''select * 
+                        from recipe
+                        limit '''+offset+ ''', '''+limit+''';'''
+
+            
+
+            # select문은, dictionary = True 를 해준다.
+            cursor = connection.cursor(dictionary = True)
+
+            cursor.execute(query)
+
+            # select문은, 아래 함수를 이용해서, 데이터를 가져온다.
+            result_list = cursor.fetchall()
+
+            print(result_list)
+
+        
+
+            # 중요! 디비에서 가져온 timestamp는 
+            # 파이썬의 datetime으로 자동으로 변경된다.
+            # 문제는! 이 데이터는 json으로 바로 보낼수 없으므로,
+            # 문자열로 바꿔서 다시 저장해서 보낸다.
+
+            i = 0
+            for record in result_list :
+                result_list[i]['created_at'] = record['created_at'].isoformat()
+                result_list[i]['updated_at'] = record['updated_at'].isoformat()
+                i = i + 1
+
+                cursor.close()
+                connection.close()
+            
+
+        except mysql.connector.Error as e :
+            print(e)
+            cursor.close()
+            connection.close()
+
+            return {"error" : str(e)}, 503
+
+        return {"result" : "success","count" : len(result_list), "result_list" : result_list}, 200
