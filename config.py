509ba812ceb9975be19634c30b4973c25aeb7e0f
ygh547 <ygh547@naver.com>
2022-06-20 17:58:31 +0900

    add

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
 class Config :
     JWT_SECRET_KEY = 'yhacademy1029##hello'
-    JWT_ACCESS_TOKEN_EXPIRES = False
+    JWT_ACCESS_TOKEN_EXPIRES = True
+    PROPAGATE_EXEPTIONS = True
--- resources/user.py
+++ resources/user.py
@@ -1,5 +1,6 @@
 from http import HTTPStatus
 from flask import request
+from flask_jwt_extended import create_access_token
 from flask_restful import Resource
 from mysql.connector.errors import Error
 from mysql_connection import get_connection
@@ -7,34 +8,35 @@ import mysql.connector
 from email_validator import validate_email, EmailNotValidError
 
 from utils import check_password, hash_password
+import datetime
 
 class UserRegisterResource(Resource) :
     def post(self) :
-
+        
         # {
         #     "username": "홍길동",
         #     "email": "abc@naver.com",
         #     "password": "1234"
         # }
 
-        # 1. 클라이언트가 body 에 보내준 json을 받아온다.
+        # 1. 클라이언트가 body 에 보내준 json 을 받아온다.
         data = request.get_json()
 
         # 2. 이메일 주소형식이 제대로 된 주소형식인지
-        # 확인하는 코드 작성. 
+        # 확인하는 코드 작성.
 
         try :
             validate_email( data['email'] )
         except EmailNotValidError as e:
             # email is not valid, exception message is human-readable
             print(str(e))
-            return {'error' : str(e)}, 400
-
-        # 3. 비밀번호의 길이가 유효한지 체크한다.
-        # 비번길이는 4자리 이상, 12자리 이하로만
-        if len(data['password']) < 4 or len(data['password']) > 12 :
-            return {'error' : '비번길이확인하세요.'}, 400
+            return {'error' : str(e)} , 400        
         
+        # 3. 비밀번호의 길이가 유효한지 체크한다.
+        # 비번길이는 4자리 이상, 12자리 이하로만!
+        if len(data['password']) < 4 or len(data['password']) > 12 :
+            return {'error' : '비번길이확인하세요'}, 400
+
         # 4. 비밀번호를 암호화 한다.
         # data['password']
 
@@ -42,31 +44,31 @@ class UserRegisterResource(Resource) :
 
         print(hashed_password)
 
-        # 5. 데이터베이스에 회원정보를 저장한다.
+        # 5. 데이터베이스에 회원정보를 저장한다!!
         try :
-            # 데이터 insert
+            # 데이터 insert 
             # 1. DB에 연결
             connection = get_connection()
-            
+
             # 2. 쿼리문 만들기
             query = '''insert into user
-                        (username, email, password)
-                        values
-                        (%s, %s, %s);'''
-                    
-            # recode 는 튜플 형태로 만든다.
-            recode = (data['username'], data['email'], hashed_password)
+                    (username, email, password)
+                    values
+                    (%s, %s , %s);'''
+            
+            record = (data['username'], data['email'], 
+                        hashed_password )
 
             # 3. 커서를 가져온다.
             cursor = connection.cursor()
 
             # 4. 쿼리문을 커서를 이용해서 실행한다.
-            cursor.execute(query, recode)
+            cursor.execute(query, record)
 
             # 5. 커넥션을 커밋해줘야 한다 => 디비에 영구적으로 반영하라는 뜻
             connection.commit()
 
-            # 5-1 디비에 저장된 아이디값 가져오기.
+            # 5-1. 디비에 저장된 아이디값 가져오기.
             user_id = cursor.lastrowid
 
             # 6. 자원 해제
@@ -78,9 +80,15 @@ class UserRegisterResource(Resource) :
             cursor.close()
             connection.close()
             return {"error" : str(e)}, 503
-        
 
-        return {'result' : 'success', 'user_id' : user_id }, 200
+        # user_id 를 바로 보내면 안되고,
+        # JWT 로 암호화 해서 보내준다.
+        # 암호화 하는 방법
+        access_token = create_access_token(user_id)
+        # access_token = create_access_token(user_id, expires_delta=datetime.timedelta(minutes=1))
+
+        return {'result' : 'success', 
+                'access_token' : access_token }, 200
 
         
 
@@ -156,10 +164,14 @@ class UserLoginResource(Resource) :
         # data['password'] 와 user_info['password']를 비교
 
         check = check_password(data['password'] , user_info['password'])
+        
 
         if check == False :
             return {'error' : '비밀번호가 맞지 않습니다.'}, 400
 
-        
+        access_token = create_access_token(user_info['id'], expires_delta=datetime.timedelta(minutes=1))
+
         return {'result' : 'success', 
-                'user_id' : user_info['id']}, 200
+                'access_token' : access_token}, 200
+        
+        
